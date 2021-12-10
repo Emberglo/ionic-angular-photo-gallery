2df02144218a25d4e44229d89de6b28167681da5
Ryan Carpenter <rccarpenter@outlook.com>
2021-12-10 13:51:08 -0700

    prompt and new page

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
+
 export class Tab3Page {
 
-  constructor() {}
+  selectedData: any;
+
+  constructor(
+    public alertController: AlertController
+    ) {}
+
+    showPrompt() {
+      this.alertController.create({
+        header: 'Prompt Alert',
+        cssClass: 'alert-style-class',
+        subHeader: 'Enter information requested',
+        message: 'Enter your first name',
+        inputs: [
+          {
+            name: 'Name',
+            type: 'text',
+          },
+        ],
+        buttons: [
+          {
+            text: 'Cancel',
+            handler: (data: any) => {
+              console.log('Canceled', data);
+            }
+          },
+          {
+            text: 'Ok',
+            handler: (data: any) => {
+              console.log('Saved Information', data);
+              this.selectedData = data;
+            }
+          }
+        ]
+      }).then(res => {
+        res.present();
+      });
+    }
 
 }
